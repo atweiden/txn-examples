3a95d98c4e1ca36e6aff98c095d83cafcf58e335
Andy Weidenbaum <archbaum@gmail.com>
2016-05-14 13:52:37 -0700

    ignore pkg artifacts, cd "$srcdir/$pkgname", test

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+_fail=0
+_dirs=($(find . -mindepth 1 \
+                -maxdepth 1 \
+                -type d ! \( -name '.git'     \
+                              -o -name '.peru'    \
+                              -o -name '.subgit'  \
+                              -o -name '.subhg'   \
+                              -o -name '.hg' \)))
+
+for _dir in "${_dirs[@]}"; do
+  pushd $_dir
+  makepkg -Acs -f
+  [[ $? != 0 ]] && _fail+=1
+  popd
+done
+
+[[ $_fail > 0 ]] && echo failed
