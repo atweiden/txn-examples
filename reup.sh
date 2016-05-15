e3104f158aa0f258f298b134491d061de36d2736
Andy Weidenbaum <archbaum@gmail.com>
2016-05-14 21:24:11 -0700

    detach-sign

--- reup.sh
+++ reup.sh
@@ -12,6 +12,6 @@ _dirs=($(find . -mindepth 1                     \
 for _dir in "${_dirs[@]}"; do
   find "$_dir" -mindepth 1 -maxdepth 1 -type f -name "*.tar.gz*" -exec rm '{}' +
   tar --exclude=PKGBUILD -cvzf "$_dir.tar.gz" "$_dir"
-  gpg -u 0x3DBB92CDB1B08DAB --armor --sign "$_dir.tar.gz"
+  gpg -u 0x3DBB92CDB1B08DAB --armor --detach-sign "$_dir.tar.gz"
   mv "$_dir".tar.gz* "$_dir"
 done
